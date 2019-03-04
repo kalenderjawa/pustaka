e1758f24634ec5d8049a7c81b33c8dd04766a5dc
Eky <prapangurakan@gmail.com>
2019-03-04 16:05:05 +0700

    Webpack config dengan Babel Loader

---
+++ webpack.config.babel.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'lib'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }]
+      }
+    ]
+  }
+}
