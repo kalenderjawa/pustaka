c09ced0b13add1953aeec25137b9a3ce8f713deb
Equan Pr <equan.p@gmail.com>
2019-04-08 15:57:58 +0700

    Modul Neptu

--- src/neptu.mjs
+++ src/neptu.mjs
@@ -1,16 +1,30 @@
 /**
- * JS EcmaScript 6
- * - neptu.js
- * Araning Tahun Ing Dalem Sewindu
+ * 📁neptu.mjs
+ * @desc Araning Tahun Ing Dalem Sewindu
  */
 
+/** @constant {string} */
 const ALIP = 'alip'
+
+/** @constant {string} */
 const EHE = 'ehe'
+
+/** @constant {string} */
 const JIMAWAL = 'jimawal'
+
+/** @constant {string} */
 const JE = 'je'
+
+/** @constant {string} */
 const DAL = 'dal'
+
+/** @constant {string} */
 const BE = 'be'
+
+/** @constant {string} */
 const WAWU = 'wawu'
+
+/** @constant {string} */
 const JIMAKIR = 'jimakir'
 
 export default { ALIP, EHE, JIMAWAL, JE, DAL, BE, WAWU, JIMAKIR }
