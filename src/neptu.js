c595c06d1a658990ef8cdd62b8867cff28da5eaa
Equan Pr <equan.p@gmail.com>
2019-04-08 11:13:19 +0700

    Neptu

---
+++ src/neptu.js
@@ -0,0 +1,16 @@
+/**
+ * JS EcmaScript 6
+ * - neptu.js
+ * Araning Tahun Ing Dalem Sewindu
+ */
+
+const ALIP = 'alip'
+const EHE = 'ehe'
+const JIMAWAL = 'jimawal'
+const JE = 'je'
+const DAL = 'dal'
+const BE = 'be'
+const WAWU = 'wawu'
+const JIMAKIR = 'jimakir'
+
+export { ALIP, EHE, JIMAWAL, JE, DAL, BE, WAWU, JIMAKIR }
