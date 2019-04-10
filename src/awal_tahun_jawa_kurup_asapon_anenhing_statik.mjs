fe8e8cb667805c78649a9515f32b4d47fc237342
Equan Pr <equan.p@gmail.com>
2019-04-10 09:05:00 +0700

    Rename sesuai dengan kurup

--- src/awal_tahun_jawa_kurup_asapon_anenhing_statik.mjs
+++ src/awal_tahun_jawa_kurup_asapon_anenhing_statik.mjs
@@ -1,65 +1,58 @@
 /**
- * 📁awal_tahun_jawa.mjs
- * @desc Hari Awal Tahun Kalender Jawa 1867 -2106 (1355-1594H, 1936-2169 M)
+ * 📁awal_tahun_jawa_kurup_asapon_anenhing_statik.mjs
+ * @desc Hari Awal Tahun Kalender Jawa 1867 -2106 (1355-1594 H, 1936-2169 M)
+ * @desc Kurup A-Sa-Pon & Kurup A-Nen-Hing
  */
 
 import Neptu from './neptu.mjs'
 import Kurup from './kurup.mjs'
 
 /**
- * Kurup Alip Selasa Pon (Asepon)
+ * Kurup Alip Selasa Pon (A-SA-PON)
  */
 
-/** @constant {object} */
 const ALIP_ASAPON = {
   neptu: Neptu.ALIP,
   kurup: Kurup.ASAPON,
   tahun: [1867, 1875, 1883, 1891, 1899, 1907, 1915, 1923, 1931, 1939, 1947, 1955, 1963, 1971, 1979]
 }
 
-/** @constant {object} */
 const EHE_ASAPON = {
   neptu: Neptu.EHE,
   kurup: Kurup.ASAPON,
   tahun: [1968, 1876, 1884, 1892, 1900, 1908, 1916, 1924, 1932, 1940, 1948, 1956, 1964, 1972, 1980]
 }
 
-/** @constant {object} */
 const JIMAWAL_ASAPON = {
   neptu: Neptu.JIMAWAL,
   kurup: Kurup.ASAPON,
   tahun: [1869, 1877, 1885, 1893, 1901, 1909, 1917, 1925, 1933, 1949, 1957, 1965, 1973, 1981]
 }
 
-/** @constant {object} */
 const JE_ASAPON = {
   neptu: Neptu.JE,
   kurup: Kurup.ASAPON,
   tahun: [1870, 1878, 1886, 1894, 1902, 1910, 1918, 1926, 1934, 1950, 1958, 1966, 1974, 1982]
 }
 
-/** @constant {object} */
 const DAL_ASAPON = {
   neptu: Neptu.DAL,
   kurup: Kurup.ASAPON,
   tahun: [1871, 1879, 1887, 1895, 1903, 1911, 1919, 1927, 1935, 1951, 1959, 1967, 1975, 1983]
 }
 
-/** @constant {object} */
 const BE_ASAPON = {
   neptu: Neptu.BE,
   kurup: Kurup.ASAPON,
   tahun: [1872, 1880, 1888, 1896, 1904, 1912, 1920, 1928, 1936, 1952, 1960, 1968, 1976, 1984]
 }
 
-/** @constant {object} */
 const WAWU_ASAPON = {
   neptu: Neptu.WAWU,
   kurup: Kurup.ASAPON,
   tahun: [1873, 1881, 1889, 1897, 1905, 1913, 1921, 1929, 1937, 1953, 1961, 1969, 1977, 1985]
 }
 
-/** @constant {object} */
 const JIMAKIR_ASAPON = {
   neptu: Neptu.JIMAKIR,
   kurup: Kurup.ASAPON,
@@ -68,7 +61,7 @@ const JIMAKIR_ASAPON = {
 }
 
 /**
- * Kurup Alip Senin Pahing
+ * Kurup Alip Senin Pahing (A-NEN-HING)
  */
 
 const ALIP_ANENHING = {
