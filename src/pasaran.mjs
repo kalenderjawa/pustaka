175952e1b67648608cd2eff7c52f035170a9d492
Equan Pr <equan.p@gmail.com>
2019-04-10 09:04:29 +0700

    Pasaran konstan

---
+++ src/pasaran.mjs
@@ -0,0 +1,21 @@
+/**
+ * 📁pasaran.mjs
+ * @desc Nama Pasaran
+ */
+
+const LEGI = new Map(['legi', 5])
+
+const PAHING = new Map(['pahing', 9])
+
+const PON = new Map(['pon', 7])
+
+const WAGE = new Map(['wage', 4])
+
+const KLIWON = new Map(['kliwon', 8])
+
+/**
+ * @desc Urutan Pasaran
+ */
+const PASARAN = [{ '1': LEGI }, { '2': PAHING }, { '3': PON }, { '4': WAGE }, { '5': KLIWON }]
+
+export default { PASARAN }
