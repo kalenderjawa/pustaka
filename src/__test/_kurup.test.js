3acf1fc48f86e1e459f880f86f1a9a864988efcf
Equan Pr <equan.p@gmail.com>
2019-07-08 15:11:26 +0700

    Fix import and testing

--- src/__test/_kurup.test.js
+++ src/__test/_kurup.test.js
@@ -1,14 +1,14 @@
-import Kurup from '../kurup.mjs'
+import * as Kurup from '../kurup.mjs'
 
 const SAMPLE_ASAPON = 'alip selasa pon'
-const SAMPLE_ANENHING = 'alip senin pahing'
+const SAMPLE_ANENHING = 'alip senen pahing'
 
 describe('TEST KURUP', () => {
   test('ASAPON', () => {
-    expect(Kurup.ASAPON).toBe(SAMPLE_ASAPON)
+    expect(`${Kurup.ASAPON.taun} ${Kurup.ASAPON.dinten} ${Kurup.ASAPON.pasaran}`).toBe(SAMPLE_ASAPON)
   })
 
   test('ANENHING', () => {
-    expect(Kurup.ANENHING).toBe(SAMPLE_ANENHING)
+    expect(`${Kurup.ANENHING.taun} ${Kurup.ANENHING.dinten} ${Kurup.ANENHING.pasaran}`).toBe(SAMPLE_ANENHING)
   })
 })
