0d70abab3642ef76a4d92d85109598652ae6ce42
Equan Pr <equan.p@gmail.com>
2019-07-08 15:50:25 +0700

    Entry template

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,15 @@
 /**
+ * ************************
+ * SENGKALA - Kalender Jawa
+ * ************************
  * 📁 index.mjs
  * Pustaka Kalender Jawa
+ * 
  */
 
 import Taun from './taun.mjs'
+import Kurup from './kurup.mjs'
+import Pasaran from './pasaran.mjs'
 
 export function test () {
   console.log(`Hello ${Taun.ALIP}`)
