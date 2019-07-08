f1a715b5e6851f291c97605d26c51aaa27e18331
Equan Pr <equan.p@gmail.com>
2019-07-08 13:43:59 +0700

    Nama bulan dalam setahun di Jawa

---
+++ src/wulan.mjs
@@ -0,0 +1,19 @@
+/**
+ * 📁wulan.mjs
+ * @desc Araning Wulan Ing Setaun
+ */
+
+const _MUKAROM = "mukarom"
+const _SAPAR = "sapar"
+const _ROBIULAWAL = "robiulawal"
+const _ROBIULAKIR = "robiulakir"
+const _JUMADILAWAL = "jumadilawal"
+const _JUMADILAKIR = "jumadilakir"
+const _ROJAB = "rojab"
+const _SAKBAN = "sakban"
+const _ROMADON = "romadon"
+const _SAWAL = "sawal"
+const _DULKODAH = "dulkodah"
+const _DULKIJAH = "dulkijah"
+
+const MUKAROM = { wulan: _MUKAROM, urutan: 1 }
