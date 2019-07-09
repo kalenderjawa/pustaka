57c68599ee46c2df1e9cf646bc778917acb48cc7
Equan Pr <equan.p@gmail.com>
2019-07-09 11:31:35 +0700

    Ubah dinten dan pasaran di kurup menjadi Object

--- src/kurup.mjs
+++ src/kurup.mjs
@@ -11,12 +11,12 @@ import * as D from './dinten.mjs'
  * @type {object} 
  * 
  */
-const ASAPON = { taun: 'alip', dinten: D.DINTEN.get(D._SELASA).dino, pasaran: P.PASARAN.get(P._PON).pasaran }
+const ASAPON = { taun: 'alip', dinten: D.DINTEN.get(D._SELASA), pasaran: P.PASARAN.get(P._PON) }
 
 /**
  * @constant
  * @type {object} 
  */
-const ANENHING = { taun: 'alip', dinten: D.DINTEN.get(D._SENEN).dino, pasaran: P.PASARAN.get(P._PAHING).pasaran }
+const ANENHING = { taun: 'alip', dinten: D.DINTEN.get(D._SENEN), pasaran: P.PASARAN.get(P._PAHING) }
 
 export { ASAPON, ANENHING }
