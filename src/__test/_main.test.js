6f09b73edac1bf814f08d8ed7e92a46fd20a1fdd
Equan Pr <equan.p@gmail.com>
2019-07-09 11:39:50 +0700

    Pakai pustaka untuk testing

--- src/__test/_main.test.js
+++ src/__test/_main.test.js
@@ -4,11 +4,14 @@
 
 import * as KalenderJawa from '../index.mjs'
 
+import { ANENHING } from '../kurup.mjs';
+import { _JIMAKIR } from '../taun.mjs';
+
 describe('Sengkala', () => {
     test("cariTaunSengkala", () => {
         return KalenderJawa.cariTaunSengkala(1994).then(r => {
-            expect(r.taun).toBe("jimakir")
-            expect(r.kurup.pasaran).toBe("pahing")
+            expect(r.taun.taun).toBe(_JIMAKIR)
+            expect(r.kurup.pasaran).toBe(ANENHING.pasaran)
         }, e => {
             expect(e).toMatch('error')
         })
