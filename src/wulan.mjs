85d93c22a1bdbd667f491c24123b5d49336818cd
Equan Pr <equan.p@gmail.com>
2019-07-12 02:33:09 +0700

    Mencoba memakai symbol - need test

--- src/wulan.mjs
+++ src/wulan.mjs
@@ -3,8 +3,13 @@
  * @desc Araning Wulan Ing Setaun
  */
 
-const _MUKAROM = "mukarom"
-const _SAPAR = "sapar"
+//const _MUKAROM = "mukarom"
+/**
+ * @constant
+ * @type {Symbol}
+ */
+const _MUKAROM = Symbol("mukarom")
+const _SAPAR = Symbol("sapar")
 const _ROBIULAWAL = "robiulawal"
 const _ROBIULAKIR = "robiulakir"
 const _JUMADILAWAL = "jumadilawal"
@@ -16,15 +21,16 @@ const _SAWAL = "sawal"
 const _DULKODAH = "dulkodah"
 const _DULKIJAH = "dulkijah"
 
-const MUKAROM = { wulan: _MUKAROM, celukan: "rom", urutan: 1 }
-const SAPAR = { wulan: _SAPAR, celukan: "par", urutan: 2 }
+
+const MUKAROM = { wulan: "mukarom", celukan: "rom", urutan: 1 }
+const SAPAR = { wulan: "sapar", celukan: "par", urutan: 2 }
 const ROBIULAWAL = { wulan: _ROBIULAWAL, celukan: "rowal", urutan: 3 }
 const ROBIULAKIR = { wulan: _ROBIULAKIR, celukan: "rokir", urutan: 4 }
 const JUMADILAWAL = { wulan: _JUMADILAWAL, celukan: "juwal", urutan: 5 }
 const JUMADILAKIR = { wulan: _JUMADILAKIR, celukan: "jukir", urutan: 6 }
 const ROJAB = { wulan: _ROJAB, celukan: "jab", urutan: 7 }
 const SAKBAN = { wulan: _SAKBAN, celukan: "ban", urutan: 8 }
-const ROMADON = { wulan: _ROMADON, celukan: "don". urutan: 9 }
+const ROMADON = { wulan: _ROMADON, celukan: "don", urutan: 9 }
 const SAWAL = { wulan: _SAWAL, celukan: "wal", urutan: 10 }
 const DULKODAH = { wulan: _DULKODAH, celukan: "dah", urutan: 11 }
 const DULKIJAH = { wulan: _DULKIJAH, celukan: "jah", urutan: 12 }
@@ -44,7 +50,7 @@ ARANING_WULAN_SETAUN.set(_SAWAL, SAWAL)
 ARANING_WULAN_SETAUN.set(_DULKODAH, DULKODAH)
 ARANING_WULAN_SETAUN.set(_DULKIJAH, DULKIJAH)
 
-export default {
+export {
     _MUKAROM,
     _SAPAR,
     _ROBIULAWAL,
