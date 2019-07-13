1b9a2766478761f87b32afb4ca7dc6a4d7a09a70
Equan Pr <equan.p@gmail.com>
2019-07-13 11:59:49 +0700

    Symbol untuk taun Be

--- src/taun.mjs
+++ src/taun.mjs
@@ -8,7 +8,7 @@ const _EHE = 'ehe'
 const _JIMAWAL = 'jimawal'
 const _JE = 'je'
 const _DAL = 'dal'
-const _BE = 'be'
+const _BE = Symbol.for('be')
 const _WAWU = 'wawu'
 const _JIMAKIR = 'jimakir'
 
@@ -17,7 +17,7 @@ const EHE = { taun: _EHE, neptu: 5, urutan: 2 }
 const JIMAWAL = { taun: _JIMAWAL, neptu: 3, urutan: 3 }
 const JE = { taun: _JE, neptu: 7, urutan: 4 }
 const DAL = { taun: _DAL, neptu: 4, urutan: 5 }
-const BE = { taun: _BE, neptu: 2, urutan: 6 }
+const BE = { taun: 'be' , neptu: 2, urutan: 6 }
 const WAWU = { taun: _WAWU, neptu: 6, urutan: 7 }
 const JIMAKIR = { taun: _JIMAKIR, neptu: 3, urutan: 8 }
 
