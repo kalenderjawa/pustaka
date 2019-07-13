4827ab8905541f54f4a392f634688f725178e555
Equan Pr <equan.p@gmail.com>
2019-07-13 19:50:32 +0700

    Registry Symbol untuk Pasaran

--- src/pasaran.js
+++ src/pasaran.js
@@ -2,17 +2,17 @@
  * 📁pasaran.js
  * @desc Pasaran & Neptu
  */
-const _LEGI = 'legi'
-const _PAHING = 'pahing'
-const _PON = 'pon'
-const _WAGE = 'wage'
-const _KLIWON = 'kliwon'
+const _LEGI = Symbol.for('legi')
+const _PAHING = Symbol.for('pahing')
+const _PON = Symbol.for('pon')
+const _WAGE = Symbol.for('wage')
+const _KLIWON = Symbol.for('kliwon')
 
-const LEGI = { pasaran: _LEGI, neptu: 5, urutan: 1 }
-const PAHING = { pasaran: _PAHING, neptu: 9, urutan: 2 }
-const PON = { pasaran: _PON, neptu: 7, urutan: 3 }
-const WAGE = { pasaran: _WAGE, neptu: 4, urutan: 4 }
-const KLIWON = { pasaran: _KLIWON, neptu: 8, urutan: 5 }
+const LEGI = { pasaran: 'legi', neptu: 5, urutan: 1 }
+const PAHING = { pasaran: 'pahing', neptu: 9, urutan: 2 }
+const PON = { pasaran: 'pon', neptu: 7, urutan: 3 }
+const WAGE = { pasaran: 'wage', neptu: 4, urutan: 4 }
+const KLIWON = { pasaran: 'kliwon', neptu: 8, urutan: 5 }
 
 const PASARAN = new Map()
 
