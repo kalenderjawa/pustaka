e46dbab982a3b75a1087b06965c604e1d366f267
Equan Pr <equan.p@gmail.com>
2019-07-13 15:37:10 +0700

    Rubah susunan variabel

--- src/rumus_apalan_awal_taun_abadi.mjs
+++ src/rumus_apalan_awal_taun_abadi.mjs
@@ -7,6 +7,29 @@ import { _MUKAROM, _SAPAR, _ROBIULAWAL, _ROBIULAKIR, _JUMADILAWAL, _JUMADILAKIR,
 import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.mjs'
 import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.mjs'
 
+
+// Rumus Matematis
+
+/**
+ * Don Nem Ro - Romadon Enem Loro
+ */
+const DON_NEM_RO = {
+    wulan: ARANING_WULAN_SETAUN.get(_ROMADON),
+    dino: 6,
+    pasaran: 2
+}
+
+/**
+ * Don Tu Pat - Romadon Pitu Papat
+ * Wulan Romadon Dino ke-Pitu (7) Pasaran ke-papat (4) 
+ */
+const DON_TU_PAT = {
+    wulan: ARANING_WULAN_SETAUN.get(_ROMADON),
+    dino: 7,
+    pasaran: 4
+}
+
+
 // Wulan Romadon Sewindu (8 Taun)
 
 /**
@@ -84,27 +107,6 @@ const DON_JIMAKIR = {
     taun: ARANING_TAHUN_SEWINDU.get(_JIMAKIR)
 }
 
-
-// Rumus Matematis
-
-/**
- * Don Nem Ro - Romadon Enem Loro
- */
-const DON_NEM_RO = {
-    wulan: ARANING_WULAN_SETAUN.get(_ROMADON),
-    dino: 6,
-    pasaran: 2
-}
-
-/**
- * Don Tu Pat - Romadon Pitu Papat
- * Wulan Romadon Dino ke-Pitu (7) Pasaran ke-papat (4) 
- */
-const DON_TU_PAT = {
-    wulan: ARANING_WULAN_SETAUN.get(_ROMADON),
-    dino: 7,
-    pasaran: 4
-}
 
 const RUMUS_APALAN_AWAL_TAUN_ABADI = new Map()
 
