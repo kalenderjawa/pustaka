fabf1c59b872e6b0f37da0f64d4579afd1a67d11
Equan Pr <equan.p@gmail.com>
2019-07-14 19:50:50 +0700

    reformat

--- src/rumus_apalan_awal_taun_abadi.js
+++ src/rumus_apalan_awal_taun_abadi.js
@@ -2,8 +2,22 @@
  * 📁rumus_apalan_awal_taun_abadi.js
  */
 
-import { _MUKAROM, _SAPAR, _ROBIULAWAL, _ROBIULAKIR, _JUMADILAWAL, _JUMADILAKIR, _ROJAB, _SAKBAN, _ROMADON, _SAWAL, _DULKODAH, _DULKIJAH, ARANING_WULAN_SETAUN
+import {
+    _MUKAROM,
+    _SAPAR,
+    _ROBIULAWAL,
+    _ROBIULAKIR,
+    _JUMADILAWAL,
+    _JUMADILAKIR,
+    _ROJAB,
+    _SAKBAN,
+    _ROMADON,
+    _SAWAL,
+    _DULKODAH,
+    _DULKIJAH,
+    ARANING_WULAN_SETAUN
 } from './wulan.js'
+
 import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
 import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
 
