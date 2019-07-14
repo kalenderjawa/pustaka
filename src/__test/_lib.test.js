6980a46e2a0dbc13d15a0ee9c4085e835e84d552
Equan Pr <equan.p@gmail.com>
2019-07-15 04:11:24 +0700

    Test browser  bundle

--- src/__test/_lib.test.js
+++ src/__test/_lib.test.js
@@ -1,7 +1,17 @@
 import * as Lib from '../../lib/index.js'
 
+const _TEST = {
+    wulan: { wulan: 'romadon', celukan: 'don', urutan: 9 },
+    taun: { taun: 'be', neptu: 2, urutan: 6 },
+    rumus: {
+      wulan: { wulan: 'romadon', celukan: 'don', urutan: 9 },
+      dino: 7,
+      pasaran: 4
+    }
+  }
+
 describe('Lib', () => {
     it('Test Lib', () => {
-        console.log(Lib.cariRumusWulanTaun('don_be'))
+        expect(Lib.cariRumusWulanTaun('don_be')).toEqual(_TEST)
     })
 })
