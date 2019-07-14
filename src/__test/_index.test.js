75d57d93d48af0643095b7066380d902a8f5eff9
Equan Pr <equan.p@gmail.com>
2019-07-15 04:11:03 +0700

    Test cariHariPasaranAwalBulan

--- src/__test/_index.test.js
+++ src/__test/_index.test.js
@@ -5,14 +5,13 @@ describe('Async Testing', () => {
         const x = await KalenderJawa.cariHariAwalBulan('romadon', 1952)
         expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
         expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
-    }, 30000)
+    })
 
-    /**
-     * This test is failed without clear reason
-    fit("cariHariAwalBulan #2", async () => {
-        const x = await KalenderJawa.cariHariAwalBulan('romadon', 1955)
-        expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
-        expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
-    }, 30000)
-    */
+    test("cariHariPasaranAwalBulan", async () => {
+        return KalenderJawa.cariHariPasaranAwalBulan('romadon', 1955).then(x => {
+            expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
+            expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
+        })           
+    })
+
 })
