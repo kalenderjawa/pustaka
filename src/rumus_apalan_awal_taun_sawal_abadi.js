1a1295542ab8497413f43edc7486e70d544aeef6
Equan Pr <equan.p@gmail.com>
2019-07-15 07:36:28 +0700

    Komen deskripsi registry

--- src/rumus_apalan_awal_taun_sawal_abadi.js
+++ src/rumus_apalan_awal_taun_sawal_abadi.js
@@ -21,6 +21,8 @@ import {
 import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
 import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
 
+// Regisrty Symbol untuk Sawal Sewindu
+
 const _WAL_ALIP = Symbol.for("wal_alip")
 const _WAL_EHE = Symbol.for("wal_ehe")
 const _WAL_JIMAWAL = Symbol.for("wal_jimawal")
