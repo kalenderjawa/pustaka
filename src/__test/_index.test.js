c62785cfba2e82c721dbb11962b6cce1823159b8
Equan Pr <equan.p@gmail.com>
2019-07-17 05:06:51 +0700

    indent 2 spasi

--- src/__test/_index.test.js
+++ src/__test/_index.test.js
@@ -1,17 +1,17 @@
 import * as KalenderJawa from '../index.js'
 
 describe('Async Testing', () => {
-    fit("cariHariAwalBulan", async () => {
-        const x = await KalenderJawa.cariHariAwalBulan('romadon', 1952)
-        expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
-        expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
-    })
+  fit("cariHariAwalBulan", async () => {
+    const x = await KalenderJawa.cariHariAwalBulan('romadon', 1952)
+    expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
+    expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
+  })
 
-    test("cariHariPasaranAwalBulan", async () => {
-        return KalenderJawa.cariHariPasaranAwalBulan('romadon', 1955).then(x => {
-            expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
-            expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
-        })           
+  test("cariHariPasaranAwalBulan", async () => {
+    return KalenderJawa.cariHariPasaranAwalBulan('romadon', 1955).then(x => {
+      expect(x.kH).toEqual({ dino: 'senen', urutan: 1 })
+      expect(x.kP).toEqual({ pasaran: 'legi', neptu: 5, urutan: 1 })
     })
+  })
 
 })
