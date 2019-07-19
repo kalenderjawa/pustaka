418086ae3fd724e7adcce4cd1f8d15246909dfdc
Equan Pr <equan.p@gmail.com>
2019-07-20 06:03:37 +0700

    fungsi untuk menggabungkan Map

---
+++ src/rupa_ati.js
@@ -0,0 +1,17 @@
+/**
+ * @flow
+ *
+ * Rupa Ati
+ * 📁 rupa_ati.js
+ */
+
+type KWMap = Map<Symbol, Object>
+
+import * as Romadon from './rumus_apalan_awal_taun_abadi.js'
+import * as Sawal from './rumus_apalan_awal_taun_sawal_abadi'
+
+function mergeMap(mapArray: Array<KWMap>): Object {
+  return new Map(function* (...mapArray) {
+    yield* mapArray
+  }())
+}
