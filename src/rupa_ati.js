5d8ef72140b82d64001206d77e9b83f60d71803c
Equan Pr <equan.p@gmail.com>
2019-07-20 06:18:16 +0700

    return flow type

--- src/rupa_ati.js
+++ src/rupa_ati.js
@@ -10,8 +10,11 @@ type KWMap = Map<Symbol, Object>
 import * as Romadon from './rumus_apalan_awal_taun_abadi.js'
 import * as Sawal from './rumus_apalan_awal_taun_sawal_abadi'
 
-function mergeMap(mapArray: Array<KWMap>): Object {
+function mergeMap(mapArray: Array<KWMap>): KWMap {
   return new Map(function* (...mapArray) {
     yield* mapArray
   }())
 }
+
+
+export { mergeMap }
