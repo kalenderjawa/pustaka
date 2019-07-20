8d0fcf661bf189d29565f64d3414dcf4f4416028
Equan Pr <equan.p@gmail.com>
2019-07-20 12:33:43 +0700

    data type untuk map pasaran

--- src/pasaran.js
+++ src/pasaran.js
@@ -12,16 +12,13 @@ const _PON = Symbol.for('pon')
 const _WAGE = Symbol.for('wage')
 const _KLIWON = Symbol.for('kliwon')
 
-/**
- * @const PasaranType
- */
 const LEGI = { pasaran: 'legi', neptu: 5, urutan: 1 }
 const PAHING = { pasaran: 'pahing', neptu: 9, urutan: 2 }
 const PON = { pasaran: 'pon', neptu: 7, urutan: 3 }
 const WAGE = { pasaran: 'wage', neptu: 4, urutan: 4 }
 const KLIWON = { pasaran: 'kliwon', neptu: 8, urutan: 5 }
 
-const PASARAN = new Map()
+const PASARAN: Map<Symbol, PasaranType> = new Map()
 
 PASARAN.set(_LEGI, LEGI)
 PASARAN.set(_PAHING, PAHING)
