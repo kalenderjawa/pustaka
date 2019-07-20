c921057bfe8fdb49929319075b21cf73c7372160
Equan Pr <equan.p@gmail.com>
2019-07-20 12:46:19 +0700

    Return type untuk fungsi cariTaunSengkala

--- src/index.js
+++ src/index.js
@@ -17,83 +17,105 @@ import { ARANING_WULAN_SETAUN } from './wulan.js'
 import { ARANING_TAHUN_SEWINDU } from './taun.js'
 import { SengkalaMap } from './rupa_ati.js'
 
+import type { PasaranType, DintenType, WulanType, TaunType, KurupType } from './type.js'
+
+type TaunKurupType = {| taun: TaunType, Kurup: KurupType, awal: Array<number>|}
+  /**
+   * Data yang dikembalikan oleh fungsi cariTaunSengkala(w,t)
+   * 
+      {
+          taun: { taun: 'jimakir', neptu: 3, urutan: 8 },
+          kurup: {
+            taun: 'alip',
+            dinten: { dino: 'senen', urutan: 1 },
+            pasaran: { pasaran: 'pahing', neptu: 9, urutan: 2 }
+          },
+          awal: [
+            1994, 2002, 2010,
+            2018, 2026, 2034,
+            2042, 2050, 2058,
+            2066, 2074, 2081,
+            2089, 2098, 2106
+          ]
+        }
+   */
+  
+
 /**
  * Mencari Taun Jawa
  * @param { number } input - 4 digit integer
  * @returns { Promise } data - hasil promise adalah object 
  */
 
-type KurupType = {}
+async function cariTaunSengkala(_q: number): Promise<TaunKurupType> {
+    return new Promise((resolve, reject) => {
+      for (let _kurup of Kurup.KURUP_ASAPON_ANENHING) {
+        _kurup.awal.find(query => {
+          if (query == _q) resolve(_kurup)
+        })
+      }
+    })
+  }
 
-async function cariTaunSengkala(_q: number): Promise<KurupType> {
-  return new Promise((resolve, reject) => {
-    for (let _kurup of Kurup.KURUP_ASAPON_ANENHING) {
-      _kurup.awal.find(query => {
-        if (query == _q) resolve(_kurup)
-      })
-    }
-  })
-}
 
 /**
  * Fungsi untuk mencari rumus perhitungan abadi
  * @param {string} wulan 
  * @param {number} taun 
  */
-async function cariRumusAbadi(wulan, taun) {
-  return new Promise((resolve, reject) => {
-    cariTaunSengkalaAwait(taun).then(r => {
-      let wulanMap = cariWulanRegistry(wulan)
-      let taunMap = cariTaunRegistry(r.taun.taun)
-
-      const KEY_RUMUS = `${wulanMap.celukan}_${taunMap.taun}`
-      resolve(cariRumusWulanTaun(KEY_RUMUS))
+async function cariRumusAbadi(wulan: string, taun: number) {
+    return new Promise((resolve, reject) => {
+      cariTaunSengkala(taun).then(r => {
+        let wulanMap = cariWulanRegistry(wulan)
+        let taunMap = cariTaunRegistry(r.taun.taun)
+        const KEY_RUMUS = `${wulanMap.celukan}_${taunMap.taun}`
+        resolve(cariRumusWulanTaun(KEY_RUMUS))
+      })
     })
-  })
-}
+  }
 
 async function cariRumusAbadiAwait(wulan, taun) {
-  let qWT = await cariRumusAbadi(wulan, taun)
-  return qWT
-}
+    let qWT = await cariRumusAbadi(wulan, taun)
+    return qWT
+  }
 
 function cariRumusWulanTaun(key) {
-  return SengkalaMap.has(Symbol.for(key)) ? SengkalaMap.get(Symbol.for(key)) : null
-}
+    return SengkalaMap.has(Symbol.for(key)) ? SengkalaMap.get(Symbol.for(key)) : null
+  }
 
 function cariTaunRegistry(taun) {
-  return ARANING_TAHUN_SEWINDU.has(Symbol.for(taun)) ? ARANING_TAHUN_SEWINDU.get(Symbol.for(taun)) : null
-}
+    return ARANING_TAHUN_SEWINDU.has(Symbol.for(taun)) ? ARANING_TAHUN_SEWINDU.get(Symbol.for(taun)) : null
+  }
 
 function cariWulanRegistry(wulan) {
-  return ARANING_WULAN_SETAUN.has(Symbol.for(wulan)) ? ARANING_WULAN_SETAUN.get(Symbol.for(wulan)) : null
-}
+    return ARANING_WULAN_SETAUN.has(Symbol.for(wulan)) ? ARANING_WULAN_SETAUN.get(Symbol.for(wulan)) : null
+  }
 
 async function konversiHariPasaran(h, p, k) {
-  let qH = await konversiHari(h, k.dinten)
-  let qP = await konversiPasaran(p, k.pasaran)
-  return { h: qH, p: qP }
-}
+    let qH = await konversiHari(h, k.dinten)
+    let qP = await konversiPasaran(p, k.pasaran)
+    return { h: qH, p: qP }
+  }
 
 async function konversiHari(h, dn) {
-  let xH = (dn + h) % 7 //Dinten MAX=7
-  if (xH == 1) { xH } else { xH = xH - 1 }
-  return new Promise((resolve, reject) => {
-    Dinten.DINTEN.forEach((value, key, map) => {
-      (value.urutan == xH) ? resolve(value) : reject(new Error('error'))
+    let xH = (dn + h) % 7 //Dinten MAX=7
+    if (xH == 1) { xH } else { xH = xH - 1 }
+    return new Promise((resolve, reject) => {
+      Dinten.DINTEN.forEach((value, key, map) => {
+        (value.urutan == xH) ? resolve(value) : reject(new Error('error'))
+      })
     })
-  })
-}
+  }
 
 async function konversiPasaran(p, ps) {
-  let xP = (ps + p) % 5 //Pasaran MAX=5
-  if (xP == 1) { xP } else { xP = xP - 1 }
-  return new Promise((resolve, reject) => {
-    PASARAN.forEach((value, key, map) => {
-      (value.urutan == xP) ? resolve(value) : reject(new Error('error'))
+    let xP = (ps + p) % 5 //Pasaran MAX=5
+    if (xP == 1) { xP } else { xP = xP - 1 }
+    return new Promise((resolve, reject) => {
+      PASARAN.forEach((value, key, map) => {
+        (value.urutan == xP) ? resolve(value) : reject(new Error('error'))
+      })
     })
-  })
-}
+  }
 
 /**
  * 
@@ -101,19 +123,19 @@ async function konversiPasaran(p, ps) {
  * @param {number} t - 4 digit integer 
  */
 async function cariHariAwalBulan(w, t) {
-  let sengkalaTaun = await cariTaunSengkala(t)
-  let sengkalaRumus = await cariRumusAbadi(w, t)
-  let kH = await konversiHari(sengkalaRumus.rumus.dino, sengkalaTaun.kurup.dinten.urutan)
-  let kP = await konversiPasaran(sengkalaRumus.rumus.pasaran, sengkalaTaun.kurup.pasaran.urutan)
+    let sengkalaTaun = await cariTaunSengkala(t)
+    let sengkalaRumus = await cariRumusAbadi(w, t)
+    let kH = await konversiHari(sengkalaRumus.rumus.dino, sengkalaTaun.kurup.dinten.urutan)
+    let kP = await konversiPasaran(sengkalaRumus.rumus.pasaran, sengkalaTaun.kurup.pasaran.urutan)
 
-  const i = { taun: sengkalaTaun.taun.taun, kurup: `${sengkalaTaun.kurup.taun} ${sengkalaTaun.kurup.dinten.dino} ${sengkalaTaun.kurup.pasaran.pasaran}` }
+    const i = { taun: sengkalaTaun.taun.taun, kurup: `${sengkalaTaun.kurup.taun} ${sengkalaTaun.kurup.dinten.dino} ${sengkalaTaun.kurup.pasaran.pasaran}` }
 
-  return { w, t, i, kH, kP }
-}
+    return { w, t, i, kH, kP }
+  }
 
 async function cariHariPasaranAwalBulan(w, t) {
-  return cariHariAwalBulan(w, t)
-}
+    return cariHariAwalBulan(w, t)
+  }
 
 export {
   cariTaunSengkala,
