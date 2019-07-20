d8cd75ffc86fb0ecc5e7acb8d6cc5e0fd785381d
Equan Pr <equan.p@gmail.com>
2019-07-20 14:02:42 +0700

    Flow type untuk data rumus

--- src/type.js
+++ src/type.js
@@ -8,5 +8,7 @@ type WulanType = {| wulan: string, celukan: string, urutan: number |}
 type TaunType = {| taun: string, neptu: number, urutan: number |}
 
 type KurupType = {| taun: string, dinten: DintenType, pasaran: PasaranType |}
+type RumusType = {| wulan: WulanType, dino: string, pasaran: number |}
 
-export type { PasaranType, DintenType, WulanType, TaunType, KurupType }
+
+export type { PasaranType, DintenType, WulanType, TaunType, KurupType, RumusType}
