c8cfed13822992b9fb5df5b5cde5b06a5d64d6a7
Equan Pr <equan.p@gmail.com>
2019-07-20 10:01:51 +0700

    export SengkalaMap dengan type Map<Symbol, Object>

--- src/rupa_ati.js
+++ src/rupa_ati.js
@@ -10,8 +10,10 @@ type KWMapType = Map<Symbol, Object>
 import * as Romadon from './rumus_apalan_awal_taun_romadon_abadi.js'
 import * as Sawal from './rumus_apalan_awal_taun_sawal_abadi'
 
-function SengkalaMap(): KWMapType {
+function _SengkalaMap(): KWMapType {
   return new Map([...Romadon.RUMUS_APALAN_AWAL_TAUN_ROMADON_ABADI, ...Sawal.RUMUS_APALAN_AWAL_TAUN_SAWAL_ABADI])
 }
+
+const SengkalaMap = _SengkalaMap()
 
 export { SengkalaMap }
