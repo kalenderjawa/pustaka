1878fded3ea71f7effee4dfbb3585db9d4aa13ea
Equan Pr <equan.p@gmail.com>
2019-07-20 20:44:56 +0700

    Registry symbol untuk Dulkodah - Alip"

---
+++ src/rumus_apalan_awal_taun_dulkodah_abadi.js
@@ -0,0 +1,31 @@
+/**
+ * @flow
+ * 
+ * 📁rumus_apalan_awal_taun_dulkodah_abadi.js
+ * Rumus untuk bulan Dulkodah selama 8 Tahun (Sewindu)
+ *
+ */
+
+import type { WulanTaunType } from './type.js'
+
+import {
+  _MUKAROM,
+  _SAPAR,
+  _ROBIULAWAL,
+  _ROBIULAKIR,
+  _JUMADILAWAL,
+  _JUMADILAKIR,
+  _ROJAB,
+  _SAKBAN,
+  _ROMADON,
+  _SAWAL,
+  _DULKODAH,
+  _DULKIJAH,
+  ARANING_WULAN_SETAUN
+} from './wulan.js'
+
+import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
+import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
+
+// Registry Symbol untuk Dulkodah Sewindu
+const _DAH_ALIP = Symbol.for("dah_alip")
