ac997ca1c88414d0ca5acb686ed59b93bc7a9f68
Equan Pr <equan.p@gmail.com>
2019-07-20 12:30:56 +0700

    Custom flow type untuk data Wulan (bulan jawa) → WulanType

--- src/type.js
+++ src/type.js
@@ -4,6 +4,7 @@
 
 type PasaranType = {| pasaran: string, neptu: number, urutan: number |}
 type DintenType = {| dino: string, urutan: number |}
+type WulanType = {| wulan: string, celukan: string, urutan: number |}
 type TaunType = {| taun: string, neptu: number, urutan: number |}
 
-export type { PasaranType, DintenType, TaunType }
+export type { PasaranType, DintenType, WulanType, TaunType }
