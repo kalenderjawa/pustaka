a3dc519420f8d708e4ad96a3f8df9720bead744d
Equan Pr <equan.p@gmail.com>
2019-07-21 18:45:53 +0700

    Build library untuk Browser dan Node.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,23 +1,26 @@
+/**
+ * Webpack Libray Build
+ * See: https://github.com/kalcifer/webpack-library-example/blob/master/webpack.config.babel.js
+ */
 const path = require('path')
+const LIB_NAME = "KalenderJawa"
 
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, 'src/index.js'),
   output: {
     path: path.join(__dirname, 'lib'),
-    filename: '[id]-[hash]-bundle.js'
+    filename: `${LIB_NAME.toLowerCase()}-[contenthash]-bundle.js`,
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    library: `${LIB_NAME}`
   },
   module: {
     rules: [
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }]
+        loader: 'babel-loader'
       }
     ]
   }
