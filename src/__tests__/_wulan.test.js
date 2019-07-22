8312f4aec96f6004b2a87f8db518daa529f02ea6
Equan Pr <equan.p@gmail.com>
2019-07-22 11:27:52 +0700

    Hapus test suites

--- src/__tests__/_wulan.test.js
+++
@@ -1,8 +0,0 @@
-import * as Wulan from '../wulan.js'
-
-describe('WULAN', () => {
-    test('_1', () => {
-        let _m = Wulan.ARANING_WULAN_SETAUN.get(Wulan._MUKAROM)
-        expect(_m.wulan).toBe('mukarom') 
-    })
-})
