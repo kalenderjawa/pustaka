0122c87c21cd89323ffc51a7331bed817e3919de
Equan Pr <equan.p@gmail.com>
2019-07-23 03:32:47 +0700

    Test suite khusus untuk bundled Library

--- src/__tests__/_lib.test.js
+++ src/__tests__/_lib.test.js
@@ -1,13 +1,26 @@
 /**
- * 
+ * Library Testing
  */
 const KalenderJawaLib = require('../../lib/kalenderjawa-2d6051b7fb0da7107ecc-bundle.js')
 
+const _TESTOBJ = {
+  taun: { taun: 'wawu', neptu: 6, urutan: 7 },
+  kurup: {
+    taun: 'alip',
+    dinten: { dino: 'selasa', urutan: 2 },
+    pasaran: { pasaran: 'pon', neptu: 7, urutan: 3 }
+  },
+  awal: [
+    1873, 1881, 1889,
+    1897, 1905, 1913,
+    1921, 1929, 1937,
+    1953, 1961, 1969,
+    1977, 1985
+  ]
+}
+
 describe("Library Testing", () => {
-    // Library testing
-  test("cariTaunSengkala", () => {
-    return KalenderJawaLib.cariTaunSengkala(1881).then(r => {
-      console.log(r)
-    })
+  test("cariTaunSengkala", async () => {
+    await expect(KalenderJawaLib.cariTaunSengkala(1881)).resolves.toMatchObject(_TESTOBJ)
   })
 })
