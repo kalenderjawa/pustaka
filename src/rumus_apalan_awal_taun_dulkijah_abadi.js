637af5a79ab65ff89ab7b9b80016285e3c3cdc6d
Equan Pr <equan.p@gmail.com>
2019-07-23 18:50:50 +0700

    Rumus untuk bulan Dulkijah

---
+++ src/rumus_apalan_awal_taun_dulkijah_abadi.js
@@ -0,0 +1,10 @@
+/**
+ * @flow
+ * 
+ * 📁rumus_apalan_awal_taun_dulkijah_abadi.js
+ * Rumus untuk bulan Dulkijah selama 8 Tahun (Sewindu)
+ *
+ */
+
+import type { WulanTaunType } from './type.js'
+
