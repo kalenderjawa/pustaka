73356c33c60243540f38e54c48e477fe38a42db0
Equan Pr <equan.p@gmail.com>
2019-07-24 07:21:18 +0700

    kode mulai untuk rumus abadi Mukarom

---
+++ src/rumus_apalan_awal_taun_mukarom_abadi.js
@@ -0,0 +1,29 @@
+/**
+ * @flow
+ * 
+ * 📁rumus_apalan_awal_taun_dulkijah_abadi.js
+ * Rumus untuk bulan Dulkijah selama 8 Tahun (Sewindu)
+ *
+ */
+
+import type { WulanTaunType } from './type.js'
+
+import {
+  _MUKAROM,
+  _SAPAR,
+  _ROBIULAWAL,
+  _ROBIULAKIR,
+  _JUMADILAWAL,
+  _JUMADILAKIR,
+  _ROJAB,
+  _SAKBAN,
+  _ROMADON,
+  _SAWAL,
+  _DULKODAH,
+  _DULKIJAH,
+  ARANING_WULAN_SETAUN
+} from './wulan.js'
+
+import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
+import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
+
