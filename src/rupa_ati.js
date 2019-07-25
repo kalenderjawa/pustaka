d6ca4c4771dc81729c12b05e927aec6caca25d06
Equan Pr <equan.p@gmail.com>
2019-07-25 16:31:50 +0700

    Merge Map Sapar ke Sangkala Map

--- src/rupa_ati.js
+++ src/rupa_ati.js
@@ -12,6 +12,7 @@ import * as Sawal from './rumus_apalan_awal_bulan_sawal_abadi'
 import * as Dulkodah from './rumus_apalan_awal_bulan_dulkodah_abadi.js'
 import * as Dulkijah from './rumus_apalan_awal_bulan_dulkijah_abadi.js'
 import * as Mukarom from './rumus_apalan_awal_bulan_mukarom_abadi.js'
+import * as Sapar from './rumus_apalan_awal_bulan_sapar_abadi.js'
 
 function _SengkalaMap(): KWMapType {
   return new Map([
@@ -19,7 +20,8 @@ function _SengkalaMap(): KWMapType {
     ...Sawal.RUMUS_APALAN_AWAL_BULAN_SAWAL_ABADI,
     ...Dulkodah.RUMUS_APALAN_AWAL_BULAN_DULKODAH_ABADI,
     ...Dulkijah.RUMUS_APALAN_AWAL_BULAN_DULKIJAH_ABADI,
-    ...Mukarom.RUMUS_APALAN_AWAL_BULAN_MUKAROM_ABADI
+    ...Mukarom.RUMUS_APALAN_AWAL_BULAN_MUKAROM_ABADI,
+    ...Sapar.RUMUS_APALAN_AWAL_BULAN_SAPAR_ABADI
   ])
 }
 
