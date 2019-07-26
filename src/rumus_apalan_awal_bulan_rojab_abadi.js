97af637dd1abac58a76f8898a05bc649a45fde2e
Equan Pr <equan.p@gmail.com>
2019-07-26 15:53:49 +0700

    Registry untuk Rojab - Jimawal

--- src/rumus_apalan_awal_bulan_rojab_abadi.js
+++ src/rumus_apalan_awal_bulan_rojab_abadi.js
@@ -29,3 +29,4 @@ import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from '.
 // Registry
 const _JAB_ALIP = Symbol.for("jab_alip")
 const _JAB_EHE = Symbol.for("jab_ehe")
+const _JAB_JIMAWAL = Symbol.for("jab_jimawal")
