429cb1b169abc5397bbff6503e6b3cb4d45ddee6
Equan Pr <equan.p@gmail.com>
2019-07-27 13:12:42 +0700

    Cleanup vars

--- src/rumus_apalan_awal_bulan_robiulawal_abadi.js
+++ src/rumus_apalan_awal_bulan_robiulawal_abadi.js
@@ -7,25 +7,8 @@
  */
 
 import type { WulanTaunType } from './type.js'
-
-import {
-  _MUKAROM,
-  _SAPAR,
-  _ROBIULAWAL,
-  _ROBIULAKIR,
-  _JUMADILAWAL,
-  _JUMADILAKIR,
-  _ROJAB,
-  _SAKBAN,
-  _ROMADON,
-  _SAWAL,
-  _DULKODAH,
-  _DULKIJAH,
-  ARANING_WULAN_SETAUN
-} from './wulan.js'
-
+import { _ROBIULAWAL, ARANING_WULAN_SETAUN } from './wulan.js'
 import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
-import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
 
 // Registry
 const _ROWAL_ALIP = Symbol.for('rowal_alip')
