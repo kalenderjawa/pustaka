1fab3222dfd341cf6da2c3958e84b2fa0a24897a
Equan Pr <equan.p@gmail.com>
2019-07-27 13:20:39 +0700

    Cleanup vars

--- src/rumus_apalan_awal_bulan_romadon_abadi.js
+++ src/rumus_apalan_awal_bulan_romadon_abadi.js
@@ -5,26 +5,9 @@
  * Rumus bulan Romadon selama 8 Tahun (Sewindu)
  */
 
-import type { RumusType, WulanTaunType } from './type.js'
-
-import {
-  _MUKAROM,
-  _SAPAR,
-  _ROBIULAWAL,
-  _ROBIULAKIR,
-  _JUMADILAWAL,
-  _JUMADILAKIR,
-  _ROJAB,
-  _SAKBAN,
-  _ROMADON,
-  _SAWAL,
-  _DULKODAH,
-  _DULKIJAH,
-  ARANING_WULAN_SETAUN
-} from './wulan.js'
-
+import type { WulanTaunType } from './type.js'
+import { _ROMADON, ARANING_WULAN_SETAUN } from './wulan.js'
 import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
-import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
 
 // Registry Wulan Romadon Sewindu (8 Taun)
 const _DON_ALIP = Symbol.for('don_alip')
--- src/rumus_apalan_awal_bulan_sakban_abadi.js
+++ src/rumus_apalan_awal_bulan_sakban_abadi.js
@@ -6,25 +6,8 @@
  */
 
 import type { WulanTaunType } from './type.js'
-
-import {
-  _MUKAROM,
-  _SAPAR,
-  _ROBIULAWAL,
-  _ROBIULAKIR,
-  _JUMADILAWAL,
-  _JUMADILAKIR,
-  _ROJAB,
-  _SAKBAN,
-  _ROMADON,
-  _SAWAL,
-  _DULKODAH,
-  _DULKIJAH,
-  ARANING_WULAN_SETAUN
-} from './wulan.js'
-
+import { _SAKBAN, ARANING_WULAN_SETAUN } from './wulan.js'
 import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
-import { _SENEN, _SELASA, _REBO, _KEMIS, _JEMAH, _SEBTU, _AKAD, DINTEN } from './dinten.js'
 
 // Registry
 const _BAN_ALIP = Symbol.for('ban_alip')
