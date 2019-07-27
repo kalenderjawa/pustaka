8ab55c07415d6370ff9d9e08666b72a58dce9193
Equan Pr <equan.p@gmail.com>
2019-07-27 13:22:15 +0700

    Standard JS

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ async function cariKurupTaun (_q: number): Promise<TaunKurupType> {
   return new Promise((resolve, reject) => {
     for (const _kurup of Kurup.KURUP_ASAPON_ANENHING) {
       _kurup.awal.find(query => {
-        if (query == _q) resolve(_kurup)
+        if (query === _q) resolve(_kurup)
       })
     }
   })
@@ -79,11 +79,11 @@ async function konversiHari (h: number, dn: number): Promise<DintenType | string
   const _xH = dn + h
   let xH = _xH % 7 // Dinten MAX=7
 
-  if (xH == 1) { xH } else { xH = xH - 1 }
+  if (xH === 1) { xH } else { xH = xH - 1 }
 
   return new Promise((resolve, reject) => {
     Dinten.DINTEN.forEach((value, key, map) => {
-      if (value.urutan == xH) {
+      if (value.urutan === xH) {
         resolve(value)
       }
     })
@@ -94,14 +94,14 @@ async function konversiPasaran (p: number, ps: number): Promise<PasaranType | st
   const _xP = ps + p
   let xP = _xP % 5 // Pasaran MAX=5
 
-  if (xP == 1) { xP } else if (xP == 0) { xP = _xP } else { xP = xP - 1 }
+  if (xP === 1) { xP } else if (xP === 0) { xP = _xP } else { xP = xP - 1 }
 
   return new Promise((resolve, reject) => {
     PASARAN.forEach((value, key, map) => {
       /**
          * Hanya mengambil value sekali, gak perlu reject selama xP masih dalam range 1-5
          */
-      if (value.urutan == xP) {
+      if (value.urutan === xP) {
         resolve(value)
       }
     })
