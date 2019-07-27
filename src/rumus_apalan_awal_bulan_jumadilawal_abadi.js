a2c772091859069495946f52710b3409ded1e1f0
Equan Pr <equan.p@gmail.com>
2019-07-27 22:31:18 +0700

    Registry, rumus Jumadilawal Sewindu

---
+++ src/rumus_apalan_awal_bulan_jumadilawal_abadi.js
@@ -0,0 +1,15 @@
+/**
+ * @flow
+ *
+ * 📁rumus_apalan_awal_bulan_jumadilawal_abadi.js
+ * Rumus untuk bulan Jumadilawal selama 8 Tahun (Sewindu)
+ *
+ */
+
+import type { WulanTaunType } from './type.js'
+import { _JUMADILAWAL, ARANING_WULAN_SETAUN } from './wulan.js'
+import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
+
+// Registry
+const _JUWAL_ALIP = Symbol.for('juwal_alip')
+
