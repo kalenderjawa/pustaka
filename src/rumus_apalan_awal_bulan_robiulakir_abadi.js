cbe9b1988a8b8acb324cd90c33c967f845eb6d61
Equan Pr <equan.p@gmail.com>
2019-07-27 17:03:17 +0700

    Robiulakir

---
+++ src/rumus_apalan_awal_bulan_robiulakir_abadi.js
@@ -0,0 +1,12 @@
+/**
+ * @flow
+ *
+ * 📁rumus_apalan_awal_bulan_robiulakir_abadi.js
+ * Rumus untuk bulan Robiulakir selama 8 Tahun (Sewindu)
+ *
+ */
+
+import type { WulanTaunType } from './type.js'
+import { _ROBIULAKIR, ARANING_WULAN_SETAUN } from './wulan.js'
+import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_SEWINDU } from './taun.js'
+
