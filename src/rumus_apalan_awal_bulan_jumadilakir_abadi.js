e097525fae7e954b8e6c6e9fe9a7dc746c88d942
Equan Pr <equan.p@gmail.com>
2019-07-28 15:01:54 +0700

    Registry jumadilakir - ehe & jumadilakir - jimawal

--- src/rumus_apalan_awal_bulan_jumadilakir_abadi.js
+++ src/rumus_apalan_awal_bulan_jumadilakir_abadi.js
@@ -12,3 +12,6 @@ import { _ALIP, _EHE, _JIMAWAL, _JE, _DAL, _BE, _WAWU, _JIMAKIR, ARANING_TAHUN_S
 
 // Registry
 const _JUKIR_ALIP = Symbol.for('jukir_alip')
+const _JUKIR_EHE = Symbol.for('jukir_ehe')
+const _JUKIR_JIMAWAL = Symbol.for('jukir_jimawal')
+
