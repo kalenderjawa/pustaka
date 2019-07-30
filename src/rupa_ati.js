f56dc2d7a07bac8670b3de547a2cd9ab6cf8e4f6
Equan Pr <equan.p@gmail.com>
2019-07-30 07:22:06 +0700

    Merge map Sakban ke Sengkala Map

--- src/rupa_ati.js
+++ src/rupa_ati.js
@@ -8,11 +8,14 @@ import * as Mukarom from './rumus_apalan_awal_bulan_mukarom_abadi.js'
 import * as Sapar from './rumus_apalan_awal_bulan_sapar_abadi.js'
 import * as Robiulawal from './rumus_apalan_awal_bulan_robiulawal_abadi.js'
 import * as Robiulakir from './rumus_apalan_awal_bulan_robiulakir_abadi.js'
+
+
+import * as Rojab from './rumus_apalan_awal_bulan_rojab_abadi.js'
+import * as Sakban from './rumus_apalan_awal_bulan_sakban_abadi.js'
 import * as Romadon from './rumus_apalan_awal_bulan_romadon_abadi.js'
 import * as Sawal from './rumus_apalan_awal_bulan_sawal_abadi.js'
 import * as Dulkodah from './rumus_apalan_awal_bulan_dulkodah_abadi.js'
 import * as Dulkijah from './rumus_apalan_awal_bulan_dulkijah_abadi.js'
-import * as Rojab from './rumus_apalan_awal_bulan_rojab_abadi.js'
 
 type KWMapType = Map<Symbol, Object>
 
@@ -26,7 +29,8 @@ function _SengkalaMap (): KWMapType {
     ...Sapar.RUMUS_APALAN_AWAL_BULAN_SAPAR_ABADI,
     ...Rojab.RUMUS_APALAN_AWAL_BULAN_ROJAB_ABADI,
     ...Robiulawal.RUMUS_APALAN_AWAL_BULAN_ROBIULAWAL_ABADI,
-    ...Robiulakir.RUMUS_APALAN_AWAL_BULAN_ROBIULAKIR_ABADI
+    ...Robiulakir.RUMUS_APALAN_AWAL_BULAN_ROBIULAKIR_ABADI,
+    ...Sakban.RUMUS_APALAN_AWAL_BULAN_SAKBAN_ABADI
   ])
 }
 
