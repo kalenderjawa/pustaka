d8d06fb3fc1a8316cf3472421c9ca2d0a716b47b
Equan Pr <equan.p@gmail.com>
2019-07-30 15:50:51 +0700

    Tambah field 'cacah' atau jumlah ke TaunType

--- src/type.js
+++ src/type.js
@@ -5,7 +5,7 @@
 type PasaranType = {| pasaran: string, neptu: number, urutan: number |}
 type DintenType = {| dino: string, urutan: number |}
 type WulanType = {| wulan: string, celukan: string, urutan: number |}
-type TaunType = {| taun: string, neptu: number, urutan: number |}
+type TaunType = {| taun: string, neptu: number, urutan: number, cacah: number |}
 
 type KurupType = {| taun: string, dinten: DintenType, pasaran: PasaranType |}
 type RumusType = {| wulan: WulanType | void, dino: number, pasaran: number |}
