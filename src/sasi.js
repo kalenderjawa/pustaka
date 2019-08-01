e0cd1c5c53c733d0e0576eda4dc4436919ded390
Equan Pr <equan.p@gmail.com>
2019-08-01 07:33:54 +0700

    Sasi RObiualkir suwene 29 dino

--- src/sasi.js
+++ src/sasi.js
@@ -26,8 +26,8 @@ const _DULKIJAH = Symbol.for('dulkijah')
 
 const MUKAROM = { wulan: 'mukarom', celukan: 'rom', urutan: 1, cacah: [30] }
 const SAPAR = { wulan: 'sapar', celukan: 'par', urutan: 2, cacah: [29] }
-const ROBIULAWAL = { wulan: 'robiulawal', celukan: 'rowal', urutan: 3, cacah:[30] }
-const ROBIULAKIR = { wulan: 'robiulakir', celukan: 'rokir', urutan: 4 }
+const ROBIULAWAL = { wulan: 'robiulawal', celukan: 'rowal', urutan: 3, cacah: [30] }
+const ROBIULAKIR = { wulan: 'robiulakir', celukan: 'rokir', urutan: 4, cacah: [29] }
 const JUMADILAWAL = { wulan: 'jumadilawal', celukan: 'juwal', urutan: 5 }
 const JUMADILAKIR = { wulan: 'jumadilakir', celukan: 'jukir', urutan: 6 }
 const ROJAB = { wulan: 'rojab', celukan: 'jab', urutan: 7 }
