58e40209de1ab81f7f3747bdf6a271bc977bd7c5
Equan Pr <equan.p@gmail.com>
2019-08-03 03:17:18 +0700

    Cleanup dan apa itu 'silpin'?

--- src/silpin.js
+++ src/silpin.js
@@ -1,5 +1,6 @@
 /**
  * @flow
+ * Silpin (Bahasa Sanskrit) artinya Seniman
  * 📁 silpin.js
  */
 // $FlowFixMe
@@ -17,9 +18,7 @@ import { SengkalaMap } from './rupa_ati.js'
 async function konversiHari (h: number, dn: number): Promise<DintenType | string> {
   const _xH = dn + h
   let xH = _xH % 7 // Dinten MAX=7
-
   if (xH !== 1) { xH = xH - 1 }
-
   return new Promise((resolve, reject) => {
     DINTEN.forEach((value, key, map) => {
       if (value.urutan === xH) {
@@ -35,9 +34,7 @@ async function konversiPasaran (p: number, ps: number): Promise<PasaranType | st
   if (xP !== 1) { if (xP === 0) { xP = _xP } else { xP = xP - 1 } }
   return new Promise((resolve, reject) => {
     PASARAN.forEach((value, key, map) => {
-      /**
-         * Hanya mengambil value sekali, gak perlu reject selama xP masih dalam range 1-5
-         */
+      // Hanya mengambil value sekali, gak perlu reject selama xP masih dalam range 1-5
       if (value.urutan === xP) {
         resolve(value)
       }
