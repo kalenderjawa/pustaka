12a12fc7245b812d226e635445a2ae7cdfb21c8b
Equan Pr <equan.p@gmail.com>
2019-08-02 10:40:13 +0700

    DATA test

---
+++ src/__tests__/_data.js
@@ -0,0 +1,14 @@
+/**
+ * @flow
+ * Test Data
+ */
+
+const _TESTDAT_2 = { i: { kurup: 'alip selasa pon', taun: 'be' }, kH: { dino: 'senen', urutan: 1 }, kP: { neptu: 5, pasaran: 'legi', urutan: 1 }, t: 1952, w: 'romadon' }
+const _TESTDAT_3 = { i: { kurup: 'alip selasa pon', taun: 'wawu' }, kH: { dino: 'senen', urutan: 1 }, kP: { neptu: 8, pasaran: 'kliwon', urutan: 5 }, t: 1881, w: 'dulkodah' }
+const _TESTDAT_4 = { i: { kurup: 'alip selasa pon', taun: 'jimawal' }, kH: { dino: 'senen', urutan: 1 }, kP: { neptu: 9, pasaran: 'pahing', urutan: 2 }, t: 1981, w: 'dulkijah' }
+
+export {
+  _TESTDAT_2 as TESTDAT2,
+  _TESTDAT_3 as TESTDAT3,
+  _TESTDAT_4 as TESTDAT4
+}
