1b8c4e1ca87b335bc7164a758ade7a4fe2ace394
Equan Pr <equan.p@gmail.com>
2019-08-03 22:32:42 +0700

    Fix assertion

--- src/__tests__/_index.test.js
+++ src/__tests__/_index.test.js
@@ -86,14 +86,14 @@ describe('Testing cariHariPasaranAwalBulanTahunJawa', () => {
   })
 
   test('Dulkodah, 1952', async () => {
-    // await expect(KalenderJawa.cariHariPasaranAwalBulanTahunJawa('dulkodah', 1952)).resolves.toStrictEqual(DAT.TESTDAT5)
-    return KalenderJawa.cariHariPasaranAwalBulanTahunJawa('dulkodah', 1952).then(d => {
-      console.log(d)
-    })
+    await expect(KalenderJawa.cariHariPasaranAwalBulanTahunJawa('dulkodah', 1952)).resolves.toStrictEqual(DAT.TEST_DULKODAH_HP_1952)
   })
 /**
   test('Dulkodah, 1881', async () => {
     await expect(KalenderJawa.cariHariPasaranAwalBulanTahunJawa('dulkodah', 1881)).resolves.toStrictEqual(DAT.TEST_DULKODAH_HP_1881)
+    return KalenderJawa.cariHariPasaranAwalBulanTahunJawa('dulkodah', 1952).then(d => {
+      console.log(d)
+    })
   })
 
   test('Dulkijah, 1981', async () => {
