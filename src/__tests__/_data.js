dd683545ccd6ec7902e5fbc5fddd9e6486fc5b68
Equan Pr <equan.p@gmail.com>
2019-08-03 22:10:44 +0700

    Test Data JUmadilawal & Jumadilakir, 1952

--- src/__tests__/_data.js
+++ src/__tests__/_data.js
@@ -34,6 +34,22 @@ const TEST_DULKIJAH_HP_1952 = {
   kP: { pasaran: 'legi', neptu: 5, urutan: 1 }
 }
 
+const TEST_JUMADILAWAL_HP_1952 = {
+  w: 'jumadilawal',
+  t: 1952,
+  i: { taun: 'be', kurup: 'alip selasa pon' },
+  kH: { dino: 'selasa', urutan: 2 },
+  kP: { pasaran: 'pon', neptu: 7, urutan: 3 }
+}
+
+const TEST_JUMADILAKIR_HP_1952 = {
+  w: 'jumadilakir',
+  t: 1952,
+  i: { taun: 'be', kurup: 'alip selasa pon' },
+  kH: { dino: 'kemis', urutan: 4 },
+  kP: { pasaran: 'pon', neptu: 7, urutan: 3 }
+}
+
 const TEST_ROMADON_HP_1952 = { i: { kurup: 'alip selasa pon', taun: 'be' }, kH: { dino: 'senen', urutan: 1 }, kP: { neptu: 5, pasaran: 'legi', urutan: 1 }, t: 1952, w: 'romadon' }
 const TEST_DULKODAH_HP_1881 = { i: { kurup: 'alip selasa pon', taun: 'wawu' }, kH: { dino: 'senen', urutan: 1 }, kP: { neptu: 8, pasaran: 'kliwon', urutan: 5 }, t: 1881, w: 'dulkodah' }
 const TEST_DULKIJAH_HP_1981 = { i: { kurup: 'alip selasa pon', taun: 'jimawal' }, kH: { dino: 'senen', urutan: 1 }, kP: { neptu: 9, pasaran: 'pahing', urutan: 2 }, t: 1981, w: 'dulkijah' }
@@ -53,8 +69,9 @@ export {
   TEST_MUKAROM_HP_1952,
   TEST_SAPAR_HP_1952,
   TEST_ROBIULAWAL_HP_1952,
+  TEST_JUMADILAWAL_HP_1952,
+  TEST_JUMADILAKIR_HP_1952,
   TEST_DULKIJAH_HP_1952,
-
   TEST_ROMADON_HP_1952,
   TEST_DULKODAH_HP_1881,
   TEST_DULKIJAH_HP_1981,
