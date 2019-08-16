393a8e1f29743b62ece3de1daeae0b6634b8d6be
Equan Pr <equan.p@gmail.com>
2019-08-16 22:53:59 +0700

    buat batasan tahun kurup

---
+++ src/batur.js
@@ -0,0 +1,17 @@
+// @flow
+// Pustaka Kalender Jawa
+// Perhitungan, pengkonversian dan penanggalan Jawa
+// 📁 batur.js
+// :ES6
+
+// saat ini Pustaka Kalender Jawa hanya berlaku untuk 2 kurup
+// prototype
+const MIN_TAUNJ = 1867
+const MAX_TAUNJ = 2106
+
+function periksaBatasan (t: number): boolean {
+  // if(t < )
+  return true
+}
+
+export { periksaBatasan }
