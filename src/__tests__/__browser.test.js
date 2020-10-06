6ca90aeed45683276ad326353bdb30a7d782551f
Equan Pr <equan.p@gmail.com>
2020-10-06 18:36:57 +0700

    default test ke non-esm browser & register on console event

--- src/__tests__/__browser.test.js
+++ src/__tests__/__browser.test.js
@@ -3,12 +3,14 @@ import "regenerator-runtime/runtime";
 
 describe("ESM Browser", () => {
   beforeEach(async () => {
-    await page.goto(PATH, { waitUntil: "load" });
+    await page.goto(`${PATH}/index-no-esm.html`, { waitUntil: "load" });
+
+    page.on("console", (consoleObj) => {
+      if (consoleObj.type() === "log") {
+        console.log(consoleObj.text());
+      }
+    });
   });
 
-  test("test HTML title", async () => {
-    await expect(page.title()).resolves.toMatch(
-      "Test Kalender Jawa Browser ESM version"
-    );
-  });
+  
 });
