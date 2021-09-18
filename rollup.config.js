6c9cba7e9c3dfe58e36099988f20e1e3a3d3c4b6
Equan Pr <equan.p@gmail.com>
2021-09-18 08:33:32 +0700

    bundle deps package.json ke build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 import json from "@rollup/plugin-json";
 import flow from "rollup-plugin-flow";
 import { terser } from "rollup-plugin-terser";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
 
 import pkg from "./package.json";
 
 export default {
   input: "src/index.js",
-  plugins: [flow(), terser(), json({ compact: true })],
+  plugins: [flow(), terser(), json({ compact: true }), nodeResolve()],
   output: [
     {
       file: pkg.module,
