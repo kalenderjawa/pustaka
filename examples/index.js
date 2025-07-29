5f788608b136a39bf1ff16e588285cfc5b113c41
Equan P <dev@junwatu.com>
2025-07-29 09:36:52 +0700

    update example

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,9 @@
 /**
- * For Node.js before v12.17.0
- * 
+ * For Node.js v12.17.0 and later with ES modules
+ *
  * ESM Flag removed since v12.17.0
  * https://github.com/nodejs/node/blob/master/doc/changelogs/CHANGELOG_V12.md#12.17.0
  */
-const KalenderJawa = require("./kalenderjawa.min.cjs");
+import * as KalenderJawa from './kalenderjawa.min.js';
 
 console.log(KalenderJawa);
